7837364d987b710eeac385ecb4a316d08ec35679
aabanakhtar <aabanakhtar.home@gmail.com>
2025-05-04 00:22:48 -0400

    RAII Unix Wrappers. Add closeRead() and closeWrite() for Pipe FD

--- src/main.cpp
+++ src/main.cpp
@@ -2,70 +2,19 @@
 #include <ranges>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "unix_wrappers.h"
 #include "util.h"
-
-enum OutputLevel
-{
-    ERROR,
-    INFO,
-    LOG
-};
-
-std::vector<char*> makeCArgs(std::vector<std::string> &args)
-{
-    std::vector<char*> cargs;
-    cargs.reserve(args.size());
-    for (auto& arg : args)
-    {
-        cargs.push_back(const_cast<char*>(arg.c_str()));
-    }
-    // required by execvp
-    cargs.push_back(nullptr);
-    return cargs;
-}
-
-constexpr std::size_t PIPE_READ = 0;
-constexpr std::size_t PIPE_WRITE = 1;
-
-void execChildProcess()
-{
-
-}
-
-void execParentProcess()
-{
-
-}
-
-void closePipe(int pipe[2])
-{
-    close(pipe[PIPE_READ]);
-    close(pipe[PIPE_WRITE]);
-}
-
-void redirect(const int pipe, const std::vector<int>& fds)
-{
-    for (const auto fd : fds)
-    {
-        dup2(pipe, fd);
-    }
-    close(pipe);
-}
 
 void eval(const std::string& input)
 {
-    int stdout_pipe_fd[2];
-    int stdin_pipe_fd[2];
-    // making the pipe failed
-    if (pipe(stdout_pipe_fd) == -1 || pipe(stdin_pipe_fd) == -1)
+    Pipe stdout_redirect, stdin_redirect;
+    if (!stdout_redirect && stdin_redirect)
     {
-        // unix way of dealing with systemwide errors
-        perror("pipe");
-        return;
+        std::cout << "Failed to create stdout/stdin pipes." << std::endl;
     }
 
-    auto args = splitString(input, ' ');
-    if (args.empty())
+    CArgv argv(splitString(input, ' '));
+    if (argv.size() == 0)
     {
         return;
     }
@@ -73,49 +22,32 @@ void eval(const std::string& input)
     pid_t pid = fork();
     if (pid == -1)
     {
-        std::cerr << "Failed to fork" << std::endl;
-        // release the resources
-        closePipe(stdout_pipe_fd);
-        closePipe(stdin_pipe_fd);
         return;
     }
     else if (pid == 0)
     {
-        // child process
-        // disable reading (we don't need), enable writing from stdout, and then close the pipe.
-        // similarly dont need writing from stdin lol
-        close(stdout_pipe_fd[PIPE_READ]);
-        close(stdin_pipe_fd[PIPE_WRITE]);
-        redirect(stdout_pipe_fd[PIPE_WRITE], {STDOUT_FILENO, STDERR_FILENO});
-        redirect(stdin_pipe_fd[PIPE_READ], {STDIN_FILENO});
+        reroute(stdout_redirect.write, {STDOUT_FILENO, STDERR_FILENO});
+        reroute(stdin_redirect.read, {STDIN_FILENO});
 
-        execvp(args[0].c_str(), makeCArgs(args).data());
+        execvp(argv.asCArgv()[0], argv.asCArgv());
         perror("execvp");
         std::cerr << "Failed to exec" << std::endl;
         exit(EXIT_FAILURE);
     }
     else
     {
-        // parent process
-        // release fd, not needed
-        close(stdout_pipe_fd[PIPE_WRITE]);
-        close(stdin_pipe_fd[PIPE_READ]);
-
         // handle input
         std::string test;
         std::getline(std::cin, test);
-        write(stdin_pipe_fd[PIPE_WRITE], test.c_str(), test.size());
-        close(stdin_pipe_fd[PIPE_WRITE]);
+        write(stdin_redirect.write.fd, test.c_str(), test.size());
 
         char buffer[4096];
         ssize_t sz;
-        while ((sz = read(stdout_pipe_fd[PIPE_READ], buffer, 4096)) > 0)
+        while ((sz = read(stdout_redirect.read.fd, buffer, 4096)) > 0)
         {
             buffer[sz] = '\0';
             std::cout << buffer << std::flush;
         }
-
-        close(stdout_pipe_fd[PIPE_READ]);
 
         // wait for process to exit
         int status;
@@ -123,7 +55,7 @@ void eval(const std::string& input)
 
         if (WIFEXITED(status))
         {
-            int code = WEXITSTATUS(status);
+            const int code = WEXITSTATUS(status);
             std::cout << "\nExited with status: " << code << std::endl;
         }
         else if (WIFSIGNALED(status))
