55b1f23287d1bb04825125fdc7a7a35aed37baa8
aabanakhtar <aabanakhtar.home@gmail.com>
2025-05-03 23:16:16 -0400

    pipe up the pippity stdeerrrr

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,50 @@ std::vector<char*> makeCArgs(std::vector<std::string> &args)
     {
         cargs.push_back(const_cast<char*>(arg.c_str()));
     }
+    // required by execvp
+    cargs.push_back(nullptr);
     return cargs;
 }
 
+constexpr std::size_t PIPE_READ = 0;
+constexpr std::size_t PIPE_WRITE = 1;
+
 void eval(const std::string& input)
 {
-    pid_t pid = fork();
-    auto args = splitString(input, ' ');
-    args.resize(args.size() + 1);
+    int stdout_pipe_fd[2];
+    // making the pipe failed
+    if (pipe(stdout_pipe_fd) == -1)
+    {
+        // unix way of dealing with systemwide errors
+        perror("pipe");
+        return;
+    }
 
+    auto args = splitString(input, ' ');
+    if (args.empty())
+    {
+        return;
+    }
+
+    pid_t pid = fork();
     if (pid == -1)
     {
         std::cerr << "Failed to fork" << std::endl;
+        // release the resources
+        close(stdout_pipe_fd[PIPE_WRITE]);
+        close(stdout_pipe_fd[PIPE_READ]);
         return;
     }
     else if (pid == 0)
     {
-        // execute the child process
+        // child process
+        // disable reading (we don't need), enable writing from stdout, and then close the pipe.
+        close(stdout_pipe_fd[PIPE_READ]);
+        dup2(stdout_pipe_fd[PIPE_WRITE], STDOUT_FILENO);
+        dup2(stdout_pipe_fd[PIPE_WRITE], STDERR_FILENO);
+        // accessible through STDOUT_FILENO
+        close(stdout_pipe_fd[PIPE_WRITE]);
+
         execvp(args[0].c_str(), makeCArgs(args).data());
         perror("execvp");
         std::cerr << "Failed to exec" << std::endl;
@@ -43,25 +70,50 @@ void eval(const std::string& input)
     }
     else
     {
+        // parent process
+        // release fd, not needed
+        close(stdout_pipe_fd[PIPE_WRITE]);
+
+        char buffer[4096];
+        ssize_t sz;
+        while ((sz = read(stdout_pipe_fd[PIPE_READ], buffer, 4096)) > 0)
+        {
+            buffer[sz] = '\0';
+            std::cout << buffer << std::flush;
+        }
+
+        close(stdout_pipe_fd[PIPE_READ]);
+
+        // wait for process to exit
         int status;
         wait(&status);
-        std::cout << "Finished process with status code: " << status << std::endl;
+
+
+        if (WIFEXITED(status))
+        {
+            int code = WEXITSTATUS(status);
+            std::cout << "Exited with status: " << code << std::endl;
+        }
+        else if (WIFSIGNALED(status))
+        {
+            std::cout << "Killed by signal: " << WTERMSIG(status) << std::endl;
+        }
     }
 }
 
 [[noreturn]] int repl()
 {
-    std::cout << "temu % ";
     for (;;)
     {
+        std::cout << "temu % ";
         std::string input;
         std::getline(std::cin, input);
         eval(input);
-        std::cout << "temu % ";
     }
 }
 
 int main()
 {
+    signal(SIGPIPE, SIG_IGN);
     return repl();
 }
