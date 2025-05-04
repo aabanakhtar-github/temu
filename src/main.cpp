56723eb7b1b13740cac1638bbecac68426364094
aabanakhtar <aabanakhtar.home@gmail.com>
2025-05-03 23:45:14 -0400

    what in the input processing

--- src/main.cpp
+++ src/main.cpp
@@ -37,11 +37,27 @@ void execParentProcess()
 
 }
 
+void closePipe(int pipe[2])
+{
+    close(pipe[PIPE_READ]);
+    close(pipe[PIPE_WRITE]);
+}
+
+void redirect(const int pipe, const std::vector<int>& fds)
+{
+    for (const auto fd : fds)
+    {
+        dup2(pipe, fd);
+    }
+    close(pipe);
+}
+
 void eval(const std::string& input)
 {
     int stdout_pipe_fd[2];
+    int stdin_pipe_fd[2];
     // making the pipe failed
-    if (pipe(stdout_pipe_fd) == -1)
+    if (pipe(stdout_pipe_fd) == -1 || pipe(stdin_pipe_fd) == -1)
     {
         // unix way of dealing with systemwide errors
         perror("pipe");
@@ -59,19 +75,19 @@ void eval(const std::string& input)
     {
         std::cerr << "Failed to fork" << std::endl;
         // release the resources
-        close(stdout_pipe_fd[PIPE_WRITE]);
-        close(stdout_pipe_fd[PIPE_READ]);
+        closePipe(stdout_pipe_fd);
+        closePipe(stdin_pipe_fd);
         return;
     }
     else if (pid == 0)
     {
         // child process
         // disable reading (we don't need), enable writing from stdout, and then close the pipe.
+        // similarly dont need writing from stdin lol
         close(stdout_pipe_fd[PIPE_READ]);
-        dup2(stdout_pipe_fd[PIPE_WRITE], STDOUT_FILENO);
-        dup2(stdout_pipe_fd[PIPE_WRITE], STDERR_FILENO);
-        // accessible through STDOUT_FILENO
-        close(stdout_pipe_fd[PIPE_WRITE]);
+        close(stdin_pipe_fd[PIPE_WRITE]);
+        redirect(stdout_pipe_fd[PIPE_WRITE], {STDOUT_FILENO, STDERR_FILENO});
+        redirect(stdin_pipe_fd[PIPE_READ], {STDIN_FILENO});
 
         execvp(args[0].c_str(), makeCArgs(args).data());
         perror("execvp");
@@ -83,6 +99,13 @@ void eval(const std::string& input)
         // parent process
         // release fd, not needed
         close(stdout_pipe_fd[PIPE_WRITE]);
+        close(stdin_pipe_fd[PIPE_READ]);
+
+        // handle input
+        std::string test;
+        std::getline(std::cin, test);
+        write(stdin_pipe_fd[PIPE_WRITE], test.c_str(), test.size());
+        close(stdin_pipe_fd[PIPE_WRITE]);
 
         char buffer[4096];
         ssize_t sz;
@@ -98,15 +121,14 @@ void eval(const std::string& input)
         int status;
         wait(&status);
 
-
         if (WIFEXITED(status))
         {
             int code = WEXITSTATUS(status);
-            std::cout << "Exited with status: " << code << std::endl;
+            std::cout << "\nExited with status: " << code << std::endl;
         }
         else if (WIFSIGNALED(status))
         {
-            std::cout << "Killed by signal: " << WTERMSIG(status) << std::endl;
+            std::cout << "\nKilled by signal: " << WTERMSIG(status) << std::endl;
         }
     }
 }
